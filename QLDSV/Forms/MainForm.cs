860885abb9c2cf67a220dd8820eab58496834925
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2024-02-11 20:37:19 +0700

    feat: first commit

---
+++ QLDSV/Forms/MainForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QLDSV.Forms {
+    public partial class MainForm : DevExpress.XtraBars.Ribbon.RibbonForm {
+        public MainForm() {
+            InitializeComponent();
+        }
+    }
+}
