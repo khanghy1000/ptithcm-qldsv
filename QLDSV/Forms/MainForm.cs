6d34dba33b73965e90c958bb31057b90e59c9e33
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2024-02-11 21:50:49 +0700

    style: reformat project

--- QLDSV/Forms/MainForm.cs
+++ QLDSV/Forms/MainForm.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿using DevExpress.XtraBars.Ribbon;
 
-namespace QLDSV.Forms {
-    public partial class MainForm : DevExpress.XtraBars.Ribbon.RibbonForm {
-        public MainForm() {
-            InitializeComponent();
-        }
+namespace QLDSV.Forms;
+
+public partial class MainForm : RibbonForm {
+    public MainForm() {
+        InitializeComponent();
     }
 }
--- QLDSV/Program.cs
+++ QLDSV/Program.cs
@@ -1,21 +1,17 @@
-﻿using DevExpress.Skins;
-using DevExpress.UserSkins;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Windows.Forms;
 using QLDSV.Forms;
 
-namespace QLDSV {
-    internal static class Program {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main() {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
-        }
+namespace QLDSV;
+
+internal static class Program {
+    /// <summary>
+    ///     The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    private static void Main() {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainForm());
     }
 }
